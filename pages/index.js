76e259f1f6dae9e3fda612ae941bbc29198c21b5
Y0suan <97809040+Y0suan@users.noreply.github.com>
2024-07-15 15:42:35 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,139 +1,119 @@
 import Eldorado from "@/Component/Eldorado";
-import Explora from "@/Component/Explora";
 import Header from "@/Component/Header";
 import Natutales from "@/Component/Natutales";
 import Gastronomia from "@/Component/Gastronomia";
-import styled from 'styled-components'
+import styled from 'styled-components';
 import Link from 'next/link';
 
-import { mongooseConnect } from "@/lib/mongoose";
-import { Product } from "@/models/Product";
-
-
-import Cafeteria from '@/Component/Categorias/Cafeterias';
-import Bares from '@/Component/Categorias/Bares';
-import Lista from '@/Component/Categorias/Lista';
-import ComidaRapida from '@/Component/Drops/ComidaRapida';
-import Heladerias from '@/Component/Drops/Heladerias';
-import Cafes from '@/Component/Drops/Cafes';
-import Restos from '@/Component/Drops/Bares';
-import Kayak from '@/Component/Drops/Kayak';
-import Senderismo from '@/Component/Drops/Senderismo';
-import Cicloturismo from '@/Component/Drops/Cicloturismo';
-import Footer from "@/Component/footer";
-
-
-
 const LinksAdicionales = styled.div`
-border-top: 1px solid #CFC9D1;
-width: 100%;
-height: auto;
-display:flex;
-flex-wrap:wrap;
-.mitadCont{
-  padding:24px;
+  border-top: 1px solid #CFC9D1;
   width: 100%;
-  height: 100%;
-  display:flex;
-  flex-direction: column;
-  align-items: center;
-  h2{
-    font-size: 2rem;
-  }
-}
-.imgCont{
-display: none;
+  height: auto;
+  display: flex;
+  flex-wrap: wrap;
 
-}
-.link{
-  display :flex;
-  align-items: center;
-  gap:8px;
-}
-@media screen and (min-width:768px){
-  height: 80vh;
-  display:flex;
-  .mitadCont{
-  width: 45%;
-  }
-  .imgCont{
+  .mitadCont {
+    padding: 24px;
+    width: 100%;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
-  width: 50%;
-  height: 100%;
-  img{
-    width:auto;
-    height: 90%;
-    border-radius: 8px;
-  }
-}
 
-}
+    h2 {
+      font-size: 2rem;
+    }
+  }
+
+  .imgCont {
+    display: none;
+  }
+
+  .link {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+
+  @media screen and (min-width: 768px) {
+    height: 80vh;
+    display: flex;
+
+    .mitadCont {
+      width: 45%;
+    }
+
+    .imgCont {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 50%;
+      height: 100%;
+
+      img {
+        width: auto;
+        height: 90%;
+        border-radius: 8px;
+      }
+    }
+  }
 `;
 
-
-
-export default function HomePage({hoteles,cafes,kayak,cicloturismo,senderismo}){
-  return(
+export default function HomePage({ hoteles, cafes, kayak, cicloturismo, senderismo }) {
+  return (
     <div>
-      <Header/>
-      {/* <Explora/> */}
-      <Eldorado/>
-        <div className="flex items-center justify-center h-screen bg-gray-100">
-      <Link href="/target-page">
-        <a>
-          <img
-            src="https://res.cloudinary.com/dzqdjsrez/image/upload/v1721049847/boton_expo_turismo_md395l.png"
-            alt="Botón Expo Turismo"
-            width={100%} // Ajusta el ancho según sea necesario
-            height={300} // Ajusta la altura según sea necesario
-            className="cursor-pointer"
-          />
-        </a>
-      </Link>
-    </div>
+      <Header />
+      <Eldorado />
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <Link href="/target-page">
+          <a>
+            <img
+              src="https://res.cloudinary.com/dzqdjsrez/image/upload/v1721049847/boton_expo_turismo_md395l.png"
+              alt="Botón Expo Turismo"
+              width="100%" // Adjust width as necessary
+              height={300} // Adjust height as necessary
+              className="cursor-pointer"
+            />
+          </a>
+        </Link>
+      </div>
       <Natutales naturales={hoteles} />
       <Gastronomia naturales={cafes} />
 
-
       <LinksAdicionales>
-          <div className='mitadCont' >
-            <h2>No te pierdas de nada </h2>
+        <div className='mitadCont'>
+          <h2>No te pierdas de nada</h2>
 
-            <div className='link'>
+          <div className='link'>
             <Kayak actividad={kayak} />
             <div>
               <h4>Kayak</h4>
-              <p>No te pierdas de nada </p>
+              <p>No te pierdas de nada</p>
             </div>
-            </div>
+          </div>
 
-            <div className='link'>
+          <div className='link'>
             <Senderismo actividad={senderismo} />
             <div>
               <h4>Senderismo</h4>
-              <p>No te pierdas de nada </p>
+              <p>No te pierdas de nada</p>
             </div>
-            </div>
-          
-            <div className='link'>
+          </div>
+
+          <div className='link'>
             <Cicloturismo actividad={cicloturismo} />
             <div>
               <h4>Cicloturismo</h4>
-              <p>No te pierdas de nada </p>
+              <p>No te pierdas de nada</p>
             </div>
-            </div>
-
           </div>
-          <div className='imgCont' >
-            <img src='https://res.cloudinary.com/dzqdjsrez/image/upload/v1688422550/agenda-next-panel/inyrov7cesjdunjm5dme.jpg' ></img>
-          </div>
-          </LinksAdicionales>
-        
-        <Footer/>
-    
+        </div>
 
+        <div className='imgCont'>
+          <img src='https://res.cloudinary.com/dzqdjsrez/image/upload/v1688422550/agenda-next-panel/inyrov7cesjdunjm5dme.jpg' alt="Additional Image" />
+        </div>
+      </LinksAdicionales>
+
+      <Footer />
     </div>
   );
 }
