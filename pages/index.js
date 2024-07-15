865e2369f7a0ca4d92509c45809544441334fcd1
Y0suan <97809040+Y0suan@users.noreply.github.com>
2024-07-15 17:13:31 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -67,17 +67,17 @@ export default function HomePage({ hoteles, cafes, kayak, cicloturismo, senderis
     <div>
       <Header />
       <Eldorado />
-      <div className="flex items-center justify-center h-screen bg-gray-100">
-        <Link href="/target-page">
-          <a>
+      <div className="flex items-center justify-center h-screen bg-gray-100 my-10">
+        <Link href="https://exp-oeldorado-im57.vercel.app/">
+          
             <img
               src="https://res.cloudinary.com/dzqdjsrez/image/upload/v1721049847/boton_expo_turismo_md395l.png"
               alt="Botón Expo Turismo"
               width="100%" // Adjust width as necessary
-              height={300} // Adjust height as necessary
+               // Adjust height as necessary
               className="cursor-pointer"
             />
-          </a>
+          
         </Link>
       </div>
       <Natutales naturales={hoteles} />
@@ -163,4 +163,6 @@ export async function getServerSideProps() {
     };
   }
 }
+
+
 
